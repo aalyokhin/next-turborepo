788f17529aa7c87b9def0dd4a111102e75491be8
Alex Alyokhin <erastonator@gmail.com>
2024-02-07 11:44:07 +0200

    wip

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,55 +1,59 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import Image from 'next/image'
+import { Card } from '@repo/ui/card'
+import { Code } from '@repo/ui/code'
+import styles from './page.module.css'
+import { Button } from '@repo/ui/button'
+import { Test } from './test'
 
 function Gradient({
   conic,
   className,
   small,
 }: {
-  small?: boolean;
-  conic?: boolean;
-  className?: string;
+  small?: boolean
+  conic?: boolean
+  className?: string
 }): JSX.Element {
+  console.log('i am here -- server component', styles)
   return (
-    <span
-      className={[
-        styles.gradient,
-        conic ? styles.glowConic : undefined,
-        small ? styles.gradientSmall : styles.gradientLarge,
-        className,
-      ]
-        .filter(Boolean)
-        .join(" ")}
-    />
-  );
+    <>
+      <Test />
+      <span
+        className={[
+          styles.gradient,
+          conic ? styles.glowConic : undefined,
+          small ? styles.gradientSmall : styles.gradientLarge,
+          className,
+        ]
+          .filter(Boolean)
+          .join(' ')}
+      />
+    </>
+  )
 }
 
 const LINKS = [
   {
-    title: "Docs",
-    href: "https://turbo.build/repo/docs",
-    description: "Find in-depth information about Turborepo features and API.",
+    title: 'Docs',
+    href: 'https://turbo.build/repo/docs',
+    description: 'Find in-depth information about Turborepo features and API.',
   },
   {
-    title: "Learn",
-    href: "https://turbo.build/repo/docs/handbook",
-    description: "Learn more about monorepos with our handbook.",
+    title: 'Learn',
+    href: 'https://turbo.build/repo/docs/handbook',
+    description: 'Learn more about monorepos with our handbook.',
   },
   {
-    title: "Templates",
-    href: "https://turbo.build/repo/docs/getting-started/from-example",
-    description: "Choose from over 15 examples and deploy with a single click.",
+    title: 'Templates',
+    href: 'https://turbo.build/repo/docs/getting-started/from-example',
+    description: 'Choose from over 15 examples and deploy with a single click.',
   },
   {
-    title: "Deploy",
-    href: "https://vercel.com/new",
-    description:
-      "Instantly deploy your Turborepo to a shareable URL with Vercel.",
+    title: 'Deploy',
+    href: 'https://vercel.com/new',
+    description: 'Instantly deploy your Turborepo to a shareable URL with Vercel.',
   },
-];
+]
 
 export default function Page(): JSX.Element {
   return (
@@ -65,7 +69,7 @@ export default function Page(): JSX.Element {
             rel="noopener noreferrer"
             target="_blank"
           >
-            By{" "}
+            By{' '}
             <Image
               alt="Vercel Logo"
               className={styles.vercelLogo}
@@ -91,7 +95,7 @@ export default function Page(): JSX.Element {
                 height={614}
                 src="circles.svg"
                 width={614}
-                style={{ pointerEvents: "none" }}
+                style={{ pointerEvents: 'none' }}
               />
             </div>
             <div className={styles.logoGradientContainer}>
@@ -105,7 +109,7 @@ export default function Page(): JSX.Element {
                 priority
                 src="turborepo.svg"
                 width={120}
-                style={{ pointerEvents: "none" }}
+                style={{ pointerEvents: 'none' }}
               />
             </div>
           </div>
@@ -140,5 +144,5 @@ export default function Page(): JSX.Element {
         ))}
       </div>
     </main>
-  );
+  )
 }
---
+++ apps/web/app/test.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+export function Test() {
+  console.log('hello from test')
+  return <div>Test</div>
+}
